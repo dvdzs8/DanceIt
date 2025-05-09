cd8254f90a8c9c69b1dc857afa6489307a9088f9
David <davidzemshi@gmail.com>
2025-05-08 20:55:34 -0400

    progress on handle key down. full screen worrk

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import vid from './assets/vid.MOV';
 import './App.css';
 
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { splitVendorChunkPlugin } from 'vite';
 
 function App() {
 
@@ -10,9 +11,41 @@ function App() {
   //useStates to sync UI
   const [playing, setPlaying] = useState(false);
   const [fullScreen, setFullScreen] = useState(false);
+  const [curTime, setCurTime] = useState(0); //in seconds
+  const [vidDuration, setVidDuration] = useState(0); //is set to correct format
 
   //get DOM references (js usable vars drawn from the HTML)
+  const vidContainerRef = useRef(null);
   const vidRef = useRef(null);
+
+  //handle keyboard inputs
+  //useeffect is called on first render and on every time one of provided dependecies (like a useState) in the array change
+  //this says: "on first render (since no dependencies), add listener for keydown to the whole window.
+  //  then, when we are done, the return function is called to cleanup the listener"
+  useEffect(() => { 
+    function handleKeyDown(myE) {
+      switch (myE.key.toLowerCase()) {
+        case " ":
+          myE.preventDefault();
+          clickPlay();
+          break;
+        case "arrowleft":
+          skip(-5);
+          break;
+        case "arrowright":
+          skip(5);
+          break;
+      }
+    }
+
+    window.addEventListener('keydown', (e) => handleKeyDown(e));
+
+    return () => window.removeEventListener('keydown', (e) => handleKeyDown(e));
+  }, []);
+
+  function skip(time) {
+    vidRef.current.currentTime += duration;
+  }
 
   function clickPlay() {
     if (!vidRef.current) return;
@@ -33,21 +66,56 @@ function App() {
     vidRef.current.muted = e.target.value === 0;
   }
 
+  function clickFullScreen() {
+
+    if (document.fullscreenElement == null) {
+      vidContainerRef.current.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+
+    //remember i still need state changes to trigger icon rerenders
+    setFullScreen(!fullScreen);
+  }
+
+  const leadingZeroFormatter = new Intl.NumberFormat(undefined, {minimumIntegerDigits: 2});
+
+  //formats both the cur time and the total duration
+  function formatTime(time) {
+
+    let strBuild = "";
+
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor(time / 60) % 60;
+    const hours = Math.floor(time / 3600);
+    if (hours === 0) {
+      return `${minutes}:${leadingZeroFormatter.format(seconds)}`;
+    } else {
+      return `${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`;
+    }
+
+  }
+
   return (
     <>
-      <div className={`video-container ${fullScreen ? 'full-screen' : ''}`}>
+      <div className={`video-container ${fullScreen ? 'full-screen' : ''}`} ref={vidContainerRef}>
 
-        <video src={vid} ref={vidRef} onEnded={clickPlay}> </video>
+        <video 
+          src={vid} 
+          ref={vidRef} 
+          onClick={clickPlay} 
+          onEnded={clickPlay}
+          onTimeUpdate={(e) => setCurTime(e.target.currentTime)}
+          onLoadedMetadata={(e) => setVidDuration(formatTime(e.target.duration))}
+        />
 
-        <br/>
+      </div>
+      <div className="video-controls-container">
 
-        <div className="video-controls-container">
-
-          <div className="timeline-container">
-
-          </div>
+          <div className="timeline-container"> </div>
 
           <div className="controls">
+
             <button className="play-button" onClick={clickPlay}>
               { playing ? "Pause" : "Play" }
             </button>
@@ -57,10 +125,14 @@ function App() {
               <input className="volume-slider" onChange={(e) => changeVolume(e)} type="range" min="0" max="1" step="any" defaultValue="1"></input>
             </div>
 
-            <button className="full-screen-button" onClick={() => setFullScreen(!fullScreen)}>{fullScreen ? "Exit" : "Full Screen"}</button>
+            <button className="full-screen-button" onClick={clickFullScreen}>{fullScreen ? "Exit" : "Full Screen"}  
+            </button>
+            
+            <div className="duration-container">
+              <p>{`${formatTime(curTime)} / ${vidDuration}`}</p>
+            </div>
 
           </div>
-        </div>
 
       </div>
 
